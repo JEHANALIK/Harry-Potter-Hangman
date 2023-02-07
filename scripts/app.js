ce51e7bd83e64f32ae68a084c69cf5ee9bbb0518
jehan ali <jeehankhosro@gmail.com>
2023-02-08 01:38:41 +0300

    reset, hangman show, game over

--- scripts/app.js
+++ scripts/app.js
@@ -8,53 +8,69 @@ function init() {
   const objectsBtn = document.querySelector("#objectsBtn");
   const start = document.querySelector("#start");
   const reset = document.querySelector("#reset");
-
-  // function handleClick(event) {
+  const hangman = document.querySelector(".hangman");
+  const popup1 = document.querySelector("#popup1");
+  let status = false;
+  let wrongLetters = 0;
   document.addEventListener("click", (event) => {
+    status = false; //set to false if the it click , then in compare to true if the same letter
     let letterClicked = event.target.classList; //click
     let letterC = event.target.innerHTML;
     if (letterClicked == "letterBtn") {
       event.target.classList.add("clicked");
       letterBtn.disabled = true;
-      // console.log(letterC);
       return compareLetter(letterC); // return the letter that you clicked
     }
   });
 
-  // }
-  // letters.innerHTML = event.target.value;
-  // console.log(letters);
-  // }
-
-  // letterBtn.forEach((btn) => {
-  //   btn.addEventListener("click", handleClick);
-  // });
-
   const spells = ["ALOHOMORA", "AVADAKEDAVRA", "FERAVERTO", "LEVIOCA"];
-  const objects = ["DELUMINATOR", "Wand", "Howler", "Revealer"];
+  const objects = ["DELUMINATOR", "WAND", "HOWLER", "REVEALER"];
   const characters = [
     "DOBBY",
     "HAGRID",
-    "Malfoy",
-    "Voldemort",
+    "MALFOY",
+    "VOLDEMORT",
     "SNAPE",
-    "Hermione",
-    "Potter",
-    "Dumbledore",
+    "HERMIONE",
+    "POTTER",
+    "DUMBLEDORE",
   ];
-  const spellSounds = [
-    "spells/alohomora.mp3",
-    "spells/avada_kedavra.mp3",
-    "spells/expecto_patronum.mp3",
-    "spells/fera_verto.mp3",
-    "spells/leviosa.mp3",
-  ];
+
+  /* ------------------------------------------reset-----------------------------------------*/
+  function resetGame() {
+    letterBtn.forEach((resetbtn) => {
+      resetbtn.disabled = true;
+    });
+    spellsBtn.disabled = true;
+    charactersBtn.disabled = true;
+    objectsBtn.disabled = true;
+    reset.disabled = true;
+    showDash.textContent = "";
+    start.disabled = false;
+    hangman.textContent = "";
+  }
+  reset.addEventListener("click", resetGame);
+  /* ------------------------------------------start-
+  maybe ill delete it----------------------------------------*/
+  function startGame() {
+    letterBtn.forEach((startbtn) => {
+      startbtn.disabled = false;
+    });
+    spellsBtn.disabled = false;
+    charactersBtn.disabled = false;
+    objectsBtn.disabled = false;
+    reset.disabled = false;
+    showDash.textContent = "";
+    start.disabled = true;
+  }
+  start.addEventListener("click", startGame);
 
   // ---------------------------------generate Random spells-----------------------------
   let spellChars = [];
   let spellDash = [];
   let spellJoin = [];
   function generateSpells() {
+    //create the dashes for the spell categorie
     var randomSpells = spells[Math.floor(Math.random() * spells.length)];
     spellChars = randomSpells.split(""); //
     spellDash = Array(randomSpells.length).fill("_");
@@ -62,164 +78,97 @@ function init() {
     console.log(spellChars);
     console.log(spellJoin);
     console.log("dashh");
-
-    // var x = "";
-
-    // for (let i = 0; i < randomSpells.length; i++) {
-    //   spellChars[i] = randomSpells[i];
-
-    //   spellDash[i] = spellChars[i].replace(/[A-Z]/, "_");
-    //   showDash.innerHTML = spellDash.join("");
-    //   // let spellChars = randomSpells.charAt(i).toUpperCase();
-    //   //spellChars.toUpperCase();
-    //   /// spellDash[i] = "_";
-    //   //x = x + " " + s;
-    //   //console.log(spellChars);
-    // }
-    // console.log(spellDash);
-    // console.log(spellChars);
-
-    // spellsBtn.disabled = true;
-    // objectsBtn.disabled = false;
-    // charactersBtn.disabled = false;
     showDash.innerHTML = spellJoin;
   }
-  //let guess = [];
-  spellsBtn.addEventListener("click", generateSpells);
-  //const dash = document.createElement("div");
+
+  spellsBtn.addEventListener("click", generateSpells); //output the correct selected letter to the dash
   function compareLetter(letterC) {
     spellChars.forEach((char, key) => {
       if (char == letterC) {
-        // guess;
-        //spellJoin[key].pop();
+        status = true;
         spellDash[key] = letterC;
         return spellDash[key];
       }
     });
     showDash.innerHTML = spellDash;
     console.log("checj");
-    //console.log(guess);
     console.log(spellDash);
     console.log(showDash.innerHTML);
+    console.log(status);
+    if (status != true) {
+      wrongLetters++;
+      hangman.classList.add(`letter-${wrongLetters}`); //add to the hangman class to display the hangman
+      document.querySelector("#wrong").play();
+      if (wrongLetters === 5) {
+        gameOver();
+        letterBtn.classList.add("stop");
+      }
+    }
+  }
+  const endText = document.querySelector("#endText");
+  const content = document.querySelector(".content");
+
+  function gameOver() {
+    endText.innerHTML = "GAME OVER!";
+    content.innerHTML = "The word is " + spellChars.join("");
+
+    document.body.appendChild(popup1);
+    resetGame();
   }
 
-  //   for (let i = 0; spellChars.length; i++) {
-  //     if (spellChars[i] == letterC) {
-  //       guess[i] = letterC;
-  //     }
-  //   }
-  // }
-  // spellChars.forEach((event, key) => {
-  //   if (event[key] === letterC) {
-
-  //     // spellJoin.innerHTML = letterC;
-  //     showDash.innerHTML = spellChars[key];
-  //   }
-  // const dash = document.createElement("div");
-  // //dash.classList.add("addDash");
-  // dash.innerHTML = letterC;
-  //const check = event.textContent;
-
-  //  event.addEventListener("click", () => {
-  // for (let i = 0; i < spellChars.length; i++) {
-  //   // showDash.appendChild(dash);
-  //   if (spellChars[i] == letterC) {
-  //     showDash.appendChild(dash);
-
-  //     // spellDash[i] = letterC;
-  //     // dash.innerHTML =dash.substring(0, i) + letterC + dash.substring(i + 1);
-  //     //  dash.innerHTML = letterC;
-  //     console.log(letterC);
-
-  //     console.log("check");
-  //   }
-  // }
-  //  });
-  //});
-  // }
+  function win() {
+    endText.innerHTML = "YOU GUESSED ALL LETTERS";
+    document.body.appendChild(popup1);
+    resetGame();
+  }
 
   // ---------------------------------generate random chracters----------------------------
-  function generateCharacters() {
-    var randomCharacters =
-      characters[Math.floor(Math.random() * characters.length)];
-    let x = "";
-    for (let i = 0; i < randomCharacters.length; i++) {
-      let characterChars = randomCharacters.charAt(i).toUpperCase();
-      // const html = `<p class="word">_</p>`;
-      characterChars.toUpperCase();
-      let s = characterChars.replace(/[A-Z]/, "_");
-      x = x + " " + s;
-    }
-    showDash.innerHTML = x;
-  }
-  charactersBtn.addEventListener("click", generateCharacters);
+  // function generateCharacters() {
+  //   var randomCharacters =
+  //     characters[Math.floor(Math.random() * characters.length)];
+  //   let x = "";
+  //   for (let i = 0; i < randomCharacters.length; i++) {
+  //     let characterChars = randomCharacters.charAt(i).toUpperCase();
+  //     // const html = `<p class="word">_</p>`;
+  //     characterChars.toUpperCase();
+  //     let s = characterChars.replace(/[A-Z]/, "_");
+  //     x = x + " " + s;
+  //   }
+  //   showDash.innerHTML = x;
+  // }
+  // charactersBtn.addEventListener("click", generateCharacters);
 
-  // ---------------------------------generate random objects----------------------------
+  /*let objectChars = [];
+  let objectDash = [];
+  let objectJoin = [];
   function generateObjects() {
+    //create the dashes for the spell categorie
     let randomObjects = objects[Math.floor(Math.random() * objects.length)];
-    let x = "";
-    for (let i = 0; i < randomObjects.length; i++) {
-      var objectChars = randomObjects.charAt(i).toUpperCase();
-      objectChars.toUpperCase();
-      // const html = `<p class="word">_</p>`;
-      let s = objectChars.replace(/[A-Z]/, "_");
-      x = x + " " + s;
-    }
-    showDash.innerHTML = x;
+    objectChars = randomObjects.split(""); //
+    objectDash = Array(randomObjects.length).fill("_");
+    objectJoin = objectDash.join(" ");
+    console.log(objectChars);
+    console.log(objectJoin);
+    console.log("dashh");
+    showDash.innerHTML = objectJoin;
   }
-  objectsBtn.addEventListener("click", generateObjects);
+  objectsBtn.addEventListener("click", generateObjects); //output the correct selected letter to the dash
+  function compareLetter(letterC) {
+    objectChars.forEach((object, i) => {
+      if (object == letterC) {
+        // guess;
+        //spellJoin[key].pop();
+        objectDash[i] = letterC;
+        return objectDash[i];
+      }
+    });
+    showDash.innerHTML = objectDash;
+    console.log("checj");
+    //console.log(guess);
+    console.log(objectDash);
+    console.log(showDash.innerHTML);
+  } */
 
-  // ---------------------------------see if correct or not---------------------------------
-  // function handleClick(event) {
-  //   for (let i = 0; i < 100; i++) {
-  //     let button = document.getElementById("button" + i);
-  //     button.addEventListener("click", function () {
-  //       console.log(event.target.i);
-  //     });
-  //   }
-  // }
-  // const keys = document.querySelectorAll('.key');
-  const checkLetter = document.querySelectorAll(".clicked").value;
-
-  // for (let i = 0; i <= spellChars.length; i++) {
-  //   if (checkLetter === spellChars[i]) {
-  //     console.log(checkLetter);
-  //   }
-  // }
-
-  // function check() {
-  //   if (button.value() === randomObjects[i]) {
-  //     let count = 5;
-  //     count--;
-  //     console.log(count);
-  //   }
-  // }
-  // letterBtn.addEventListener("click", check);
-
-  // function printLetter() {
-  //   for (var i = 0; i < characterChars.length; i++) {
-  //     if (characterChars[i] ==
-
-  //     var objectFill = document.createTextNode(randomObjects[i]);
-  //     letters.appendChild(objectFill);
-  //   }
-  // }
-  // letterBtn.addEventListener("click", printLetter);
-
-  // ---------------------------------alphabets---------------------------------
-
-  // for (let i = 65; i <= 90; i++) {
-  //   var letterBtn = document.createElement("button");
-  //   // lettersShow += `<button type='button' class='letterBtn'>${String.fromCharCode(
-  //   //   i
-  //   // )}</button>`;
-  //   letterBtn.innerHTML = String.fromCharCode(i);
-  //   letters.append(letterBtn);
-  // }
-  // ---------------------------------guess the letter---------------------------------
-  // if (letterBtn.value == randomCharacters[i]) {
-  //  showDash.innerHTML = randomCharacters[i];
-  // }
   const audio = document.querySelector("#audio");
   playTheme.addEventListener("click", (event) => {
     audio.src = "/Harry Potter Theme Song.mp3";
